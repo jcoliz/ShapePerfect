fc965a8229186370cc9f87310d0c02c08e2ef925
James Coliz <jcoliz@outlook.com>
2024-06-05 19:44:07 -0700

    test: Failing test for dump shapes

--- ShapePerfect.Tests/DumperTests.cs
+++ ShapePerfect.Tests/DumperTests.cs
@@ -1,4 +1,5 @@
 using System.Reflection;
+using System.Text;
 using ShapeCrawler;
 using ShapePerfect.Lib;
 
@@ -27,6 +28,28 @@ public class DumperTests
         dumper.Load(pres);
 
         Assert.That(dumper.Shapes, Has.Count.EqualTo(8));
+    }
+
+    /// <summary>
+    /// Scenario: Can dump results of load to Toml
+    /// </summary>
+    [Test]
+    [Explicit]
+    public void DumpShapes()
+    {
+        var dumper = new Dumper();
+        var pres = Load<IPresentation>("chaos.pptx");
+        dumper.Load(pres);
+
+        var stringBuilder = new StringBuilder();
+        using (TextWriter writer = new StringWriter(stringBuilder))
+        {
+            dumper.DumpToml(writer);
+        }
+        var lines = stringBuilder.ToString().Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
+
+        // Each shape should be 4 lines
+        Assert.That(lines, Has.Length.EqualTo(32));
     }
 
     private T Load<T>(string name) where T : class
